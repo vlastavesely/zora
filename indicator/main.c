66f0af187cc84961753e236abb7ac6fab89a5893
Vlasta Vesely <vlastavesely@protonmail.ch>
2022-04-06 15:02:39 +0200

    main.c: cleanup

--- indicator/main.c
+++ indicator/main.c
@@ -6,43 +6,21 @@
  *
  * There's ABSOLUTELY NO WARRANTY, express or implied.
  */
-#include <libayatana-appindicator/app-indicator.h>
-#include <zora/randr.h>
 #include "indicator.h"
-
-struct zora_state {
-	struct zora_randr_state state;
-	ZoraIndicator *indicator;
-};
-
-static int initialise(struct zora_state *state)
-{
-	zora_randr_initialise(&state->state);
-	state->indicator = zora_indicator_new();
-	return 0;
-}
-
-static int finalise(struct zora_state *state)
-{
-	zora_randr_finalise(&state->state);
-	zora_indicator_free(state->indicator);
-	return 0;
-}
 
 int main(int argc, char **argv)
 {
-	struct zora_state state;
-	int ret;
+	ZoraIndicator *indicator;
 
 	gtk_init(&argc, &argv);
 
-	ret = initialise(&state);
-	if (ret != 0)
-		return ret;
+	indicator = zora_indicator_new();
+	if (indicator == NULL)
+		return -1;
 
 	gtk_main();
 
-	finalise(&state);
+	zora_indicator_free(indicator);
 
 	return 0;
 }
